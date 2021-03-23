75dff31f8366e49fc6ac80188222822b4642ac23
Jevon Cochran <jevon.cochran@gmail.com>
2021-03-23 16:17:12 -0700

    test SelectedProduct

--- client/pages/signup/[item].js
+++ client/pages/signup/[item].js
@@ -1,3 +1,4 @@
+import { apiRoot } from "../../utils/axios-config";
 import SelectedProduct from "../../components/customerView/SignUp/SelectedProduct";
 
 const SelectedProductPage = () => {
@@ -5,3 +6,20 @@ const SelectedProductPage = () => {
 };
 
 export default SelectedProductPage;
+
+export async function getStaticPaths() {
+  const res = await fetch(`${apiRoot}/products`);
+  const products = await res.json();
+
+  const paths = products.map((product) => ({
+    params: { item: product.route },
+  }));
+
+  return {
+    paths: [
+      { params: { item: products[0].route } },
+      { params: { item: products[1].route } },
+    ],
+    fallback: false,
+  };
+}
