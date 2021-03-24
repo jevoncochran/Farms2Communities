61352f89598ddb6f25fc36b6862c1bb70d37ca84
Jevon Cochran <jevon.cochran@gmail.com>
2021-03-24 16:00:41 -0700

    test to see if image error fixed

--- client/next.config.js
+++ client/next.config.js
@@ -1,38 +1,38 @@
 const withImages = require("next-images");
-module.exports = withImages({
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    // Important: return the modified config
+// module.exports = withImages({
+//   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+//     // Note: we provide webpack above so you should not `require` it
+//     // Perform customizations to webpack config
+//     // Important: return the modified config
 
-    // Example using webpack option
-    //config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
-    config.node = {
-      fs: "empty",
-      net: "empty",
-      tls: "empty",
-    };
-    return config;
-  },
-  webpackDevMiddleware: (config) => {
-    // Perform customizations to webpack dev middleware config
-    // Important: return the modified config
-    return config;
-  },
-});
+//     // Example using webpack option
+//     //config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+//     config.node = {
+//       fs: "empty",
+//       net: "empty",
+//       tls: "empty",
+//     };
+//     return config;
+//   },
+//   webpackDevMiddleware: (config) => {
+//     // Perform customizations to webpack dev middleware config
+//     // Important: return the modified config
+//     return config;
+//   },
+// });
 
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//     // Fixes npm packages that depend on `fs` module
+//     if (!isServer) {
+//       config.node = {
+//         fs: "empty",
+//       };
+//     }
 
-    return config;
-  },
-};
+//     return config;
+//   },
+// };
 
-// module.exports = withImages();
+module.exports = withImages();
