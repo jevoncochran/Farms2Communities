5d0659608a3069834e1d20440048fc2263ee0c06
Jevon Cochran <jevon.cochran@gmail.com>
2021-03-31 17:39:29 -0700

    test2

--- client/pages/checkout.js
+++ client/pages/checkout.js
@@ -1,7 +1,13 @@
 import Checkout from "../components/customerView/Checkout";
+import dynamic from "next/dynamic";
 
-const CheckoutPage = () => {
-  return <Checkout />;
-};
+// const CheckoutPage = () => {
+//   return <Checkout />;
+// };
+
+const CheckoutPage = dynamic(
+  () => import("../components/customerView/Checkout"),
+  { ssr: false }
+);
 
 export default CheckoutPage;
